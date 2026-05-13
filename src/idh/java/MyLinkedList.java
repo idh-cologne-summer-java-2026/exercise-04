534e475ebfd9f8367d976191ff8b2bf6be94aabf
Nils Reiter <nils.reiter@uni-koeln.de>
2026-05-13 11:05:41 +0200

    base code

---
+++ src/idh/java/MyLinkedList.java
@@ -0,0 +1,103 @@
+package idh.java;
+
+public class MyLinkedList<T> {
+
+	/**
+	 * Helper class for the list elements
+	 */
+	private class ListElement {
+		T payload;
+		ListElement next = null;
+
+		ListElement(T value) {
+			this.payload = value;
+		}
+	}
+
+	/**
+	 * We only need to store the very first element of our list, because it will
+	 * know whether there is a next element.
+	 */
+	ListElement first;
+
+	public int size() {
+		// TODO: Implement
+		return 0;
+	}
+
+	public boolean contains(Object o) {
+		// TODO: Implement
+		return false;
+	}
+
+	public boolean remove(Object o) {
+		// TODO: Implement
+		return false;
+
+	}
+
+	public T set(int index, T element) {
+		// TODO: Implement
+		return element;
+	}
+
+	public void add(int index, T element) {
+		// TODO: Implement
+	}
+
+
+	public T remove(int index) {
+		// TODO: Implement
+		return null;
+	}
+
+	public boolean isEmpty() {
+		return first == null;
+	}
+
+	public void clear() {
+		first = null;
+	}
+
+	public T get(int index) {
+		return getElement(index).payload;
+	}
+
+	/**
+	 * Internal method that iterates over the list, returning the last element
+	 * (i.e., the one whose next field is null)
+	 * 
+	 * @return
+	 */
+	private ListElement last() {
+		if (first == null)
+			return null;
+		ListElement current = first;
+
+		while (current.next != null) {
+			current = current.next;
+		}
+		return current;
+	}
+
+	/**
+	 * Internal method to get the list element (not the value) of the list at the
+	 * specified index position.
+	 * 
+	 * @param index
+	 * @return
+	 */
+	private ListElement getElement(int index) {
+		if (isEmpty())
+			return null;
+		ListElement current = first;
+		while (current != null) {
+			if (index == 0)
+				return current;
+			index--;
+			current = current.next;
+		}
+		return null;
+	}
+
+}
