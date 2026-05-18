ea4785ead7af4c7f1f30ecb9669589be759c1565
Patty <patrickscheuerkev@gmail.com>
2026-05-18 14:55:54 +0200

    exercise-04

--- src/idh/java/MyLinkedList.java
+++ src/idh/java/MyLinkedList.java
@@ -21,34 +21,73 @@ public class MyLinkedList<T> {
 	ListElement first;
 
 	public int size() {
-		// TODO: Implement
-		return 0;
+	    int count = 0;
+	    ListElement current = first;
+	    while (current != null) {
+	        count++;
+	        current = current.next;
+	    }
+	    return count;
 	}
 
 	public boolean contains(Object o) {
-		// TODO: Implement
-		return false;
+	    ListElement current = first;
+	    while (current != null) {
+	        if (current.payload.equals(o)) return true;
+	        current = current.next;
+	    }
+	    return false;
 	}
 
 	public boolean remove(Object o) {
-		// TODO: Implement
-		return false;
-
+	    if (first == null) return false;
+	    if (first.payload.equals(o)) {
+	        first = first.next;
+	        return true;
+	    }
+	    ListElement current = first;
+	    while (current.next != null) {
+	        if (current.next.payload.equals(o)) {
+	            current.next = current.next.next;
+	            return true;
+	        }
+	        current = current.next;
+	    }
+	    return false;
 	}
 
 	public T set(int index, T element) {
-		// TODO: Implement
-		return element;
+	    ListElement node = getElement(index);
+	    T oldPayload = node.payload;
+	    node.payload = element;
+	    return oldPayload;
 	}
 
 	public void add(int index, T element) {
-		// TODO: Implement
+	    ListElement newNode = new ListElement(element);
+	    if (index == 0) {
+	        newNode.next = first;
+	        first = newNode;
+	    } else {
+	        ListElement prev = getElement(index - 1);
+	        newNode.next = prev.next;
+	        prev.next = newNode;
+	    }
 	}
 
 
 	public T remove(int index) {
-		// TODO: Implement
-		return null;
+	    if (isEmpty()) return null;
+	    T removedValue;
+	    if (index == 0) {
+	        removedValue = first.payload;
+	        first = first.next;
+	    } else {
+	        ListElement prev = getElement(index - 1);
+	        removedValue = prev.next.payload;
+	        prev.next = prev.next.next;
+	    }
+	    return removedValue;
 	}
 
 	public boolean isEmpty() {
