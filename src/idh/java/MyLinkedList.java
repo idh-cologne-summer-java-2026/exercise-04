6b7324c9fc0c125587e71428bb2ad91327749289
René <Ren@172.29.238.169>
2026-05-19 13:14:46 +0200

    Erste Version Hausaufgabe # WARNING: head commit changed in the meantime
    hi

--- src/idh/java/MyLinkedList.java
+++ src/idh/java/MyLinkedList.java
@@ -2,14 +2,15 @@ package idh.java;
 
 public class MyLinkedList<T> {
 
-	// black-cat-ninja branch
 	/**
 	 * Helper class for the list elements
 	 */
+	//Deklaration
 	private class ListElement {
 		T payload;
 		ListElement next = null;
 
+		//Konstruktor 
 		ListElement(T value) {
 			this.payload = value;
 		}
@@ -22,33 +23,91 @@ public class MyLinkedList<T> {
 	ListElement first;
 
 	public int size() {
-		// TODO: Implement
-		return 0;
+		int count = 0; 
+		ListElement current = first;
+		while (current != null) {
+			count++;
+			current = current.next;
+		}
+		return count;
 	}
 
 	public boolean contains(Object o) {
-		// TODO: Implement
+		ListElement current = first;
+		while (current != null) {
+			if (current.payload == o) {
+				return true;
+			}
+			current = current.next;
+		}
+		
 		return false;
 	}
 
+	// Hier muss der Pfeil(also next Object) verändert werden, damit der Pfeil nicht mehr auf das entfernte Objekt zeigt, sondern auf das 
+	// übernächste 
 	public boolean remove(Object o) {
-		// TODO: Implement
+		
+		//Erstes Elemet entfernt (Muss spezifisch genannt werden, da es sonst keinen next-Pfeil gibt, der geändert werden könnte
+		if (o.equals(first.payload)) {
+			first = first.next;
+			return true;
+		}
+		
+		//Element aus der Mitte entfernt
+		ListElement current = first;
+		while (current.next != null) {
+			if (o.equals(current.next.payload)) {
+				current.next = current.next.next;
+				return true;
+			}
+			current = current.next; 
+		}
 		return false;
-
 	}
 
 	public T set(int index, T element) {
-		// TODO: Implement
+		// getElement sucht uns vorher schon das richtige Element raus 
+		//Hier wird quasi nur der String überschrieben, an der Pfeilkette (next) ändert sich nichts 
+		ListElement node = getElement(index);
+		node.payload = element; 
 		return element;
 	}
 
 	public void add(int index, T element) {
-		// TODO: Implement
+		ListElement newElement = new ListElement(element);
+		
+		//Erstes Element
+		if (index == 0) {
+			newElement.next = first;
+			first = newElement;
+		} else {
+			ListElement previous = getElement (index-1);
+			if (previous != null) {
+				newElement.next = previous.next;
+				previous.next = newElement; 
+			}
+		}
+		
+		ListElement previous = getElement (index-1);
+		// Hier müssen noch die next-Pfeile geändert werden 
+		previous.next =newElement; //(Der vorherige Pfeil zeigt aufs neue Elment)
+		newElement.next = previous.next; // (Das neue Element braucht den alten Pfeil)
+		
 	}
 
 
 	public T remove(int index) {
-		// TODO: Implement
+		//Erstes Element
+		if (index == 0) {
+			first = first.next;
+		}
+		
+		//Element aus der Mitte
+		//Vorgänger des gefragten Elements raussuchen 
+		ListElement previous = getElement(index-1);
+		//Der next-Pfeil muss geändert werden auf den übernächsten Wert 
+		previous.next = previous.next.next;
 		return null;
 	}
 
@@ -70,7 +129,7 @@ public class MyLinkedList<T> {
 	 * 
 	 * @return
 	 */
-	private ListElement last() {
+	/*private ListElement last() {
 		if (first == null)
 			return null;
 		ListElement current = first;
@@ -79,7 +138,7 @@ public class MyLinkedList<T> {
 			current = current.next;
 		}
 		return current;
-	}
+	}*/
 
 	/**
 	 * Internal method to get the list element (not the value) of the list at the
