086297a0b6a07a5ae1b45aea650982cbfe729fad
Niklas5 <206838332+Niklas5@users.noreply.github.com>
2026-05-19 23:41:23 +0200

    exercise-04

--- src/idh/java/MyLinkedList.java
+++ src/idh/java/MyLinkedList.java
@@ -21,34 +21,100 @@ public class MyLinkedList<T> {
 	ListElement first;
 
 	public int size() {
-		// TODO: Implement
-		return 0;
+		int count = 0;
+		ListElement current = first;
+
+		while (current != null) {
+			count++;
+			current = current.next;
+		}
+
+		return count;
 	}
 
 	public boolean contains(Object o) {
-		// TODO: Implement
+		ListElement current = first;
+
+		while (current != null) {
+			if (o == null ? current.payload == null : o.equals(current.payload)) {
+				return true;
+			}
+			current = current.next;
+		}
+
 		return false;
 	}
 
 	public boolean remove(Object o) {
-		// TODO: Implement
+		if (first == null) {
+			return false;
+		}
+
+		if (o == null ? first.payload == null : o.equals(first.payload)) {
+			first = first.next;
+			return true;
+		}
+
+		ListElement current = first;
+
+		while (current.next != null) {
+			if (o == null ? current.next.payload == null : o.equals(current.next.payload)) {
+				current.next = current.next.next;
+				return true;
+			}
+			current = current.next;
+		}
+
 		return false;
 
 	}
 
 	public T set(int index, T element) {
-		// TODO: Implement
-		return element;
+		ListElement current = getElement(index);
+
+		if (current == null) {
+			throw new IndexOutOfBoundsException();
+		}
+
+		T oldValue = current.payload;
+		current.payload = element;
+		return oldValue;
 	}
 
 	public void add(int index, T element) {
-		// TODO: Implement
+		if (index < 0 || index > size()) {
+			throw new IndexOutOfBoundsException();
+		}
+
+		ListElement newElement = new ListElement(element);
+
+		if (index == 0) {
+			newElement.next = first;
+			first = newElement;
+			return;
+		}
+
+		ListElement previous = getElement(index - 1);
+		newElement.next = previous.next;
+		previous.next = newElement;
 	}
 
 
 	public T remove(int index) {
-		// TODO: Implement
-		return null;
+		if (index < 0 || index >= size()) {
+			throw new IndexOutOfBoundsException();
+		}
+
+		if (index == 0) {
+			T value = first.payload;
+			first = first.next;
+			return value;
+		}
+
+		ListElement previous = getElement(index - 1);
+		T value = previous.next.payload;
+		previous.next = previous.next.next;
+		return value;
 	}
 
 	public boolean isEmpty() {
@@ -60,7 +126,13 @@ public class MyLinkedList<T> {
 	}
 
 	public T get(int index) {
-		return getElement(index).payload;
+		ListElement element = getElement(index);
+
+		if (element == null) {
+			throw new IndexOutOfBoundsException();
+		}
+
+		return element.payload;
 	}
 
 	/**
@@ -88,8 +160,9 @@ public class MyLinkedList<T> {
 	 * @return
 	 */
 	private ListElement getElement(int index) {
-		if (isEmpty())
+		if (index < 0 || isEmpty())
 			return null;
+
 		ListElement current = first;
 		while (current != null) {
 			if (index == 0)
