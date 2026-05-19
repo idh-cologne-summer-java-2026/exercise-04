8846d4a40a62fa50c384959c8cdc24c45fc19239
yonca <yonca@Yonca.fritz.box>
2026-05-19 21:45:52 +0200

    Hausaufgaben versuch

--- src/idh/java/MyLinkedList.java
+++ src/idh/java/MyLinkedList.java
@@ -21,28 +21,86 @@ public class MyLinkedList<T> {
 	ListElement first;
 
 	public int size() {
-		// TODO: Implement
-		return 0;
+		int count =0;
+		ListElement current = first;
+		while(current != null) {
+			count++;
+			current = current.next;
+		}return count;
+		
+		
 	}
 
 	public boolean contains(Object o) {
-		// TODO: Implement
+		
+		
+		
+		
+		ListElement current = first;
+	 while( current !=null) {
+		if(current.payload== o) {
+	return true;	
+		}
+		 current =current.next;
+		 
+		  }
 		return false;
 	}
 
 	public boolean remove(Object o) {
-		// TODO: Implement
+		
+      if(o.equals(first.payload)) {
+			
+			first = first.next;
+			return true;
+		}
+		
+		
+		ListElement current =first;
+		while(current.next != null) {
+			
+			if(o.equals(current.next.payload)) {
+				
+				current.next = current.next.next;
+				return true;
+			}
+			
+			current = current.next;
+		}
+		
+		
+		
 		return false;
 
 	}
 
 	public T set(int index, T element) {
-		// TODO: Implement
-		return element;
+		
+		ListElement node = getElement(index);
+		T oldValue = node.payload;
+		node.payload = element;
+		return oldValue;
+		
+		
+		
 	}
 
 	public void add(int index, T element) {
-		// TODO: Implement
+		ListElement newElement = new ListElement(element);
+		
+		if(index ==0) {
+			newElement.next = first;
+			first = newElement;
+		}else {
+		
+		ListElement previous = getElement(index-1);
+		newElement.next= previous.next;
+		if(previous  !=null) {
+		previous.next = previous.next;
+		previous.next= newElement;
+		}	
+		
+		}	
 	}
 
 
