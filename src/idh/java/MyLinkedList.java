96c51ebcd01979edb73006970d69ce16967ea3a0
test <test@DESKTOP-C0QB574>
2026-05-19 20:20:56 +0200

    20:20...

--- src/idh/java/MyLinkedList.java
+++ src/idh/java/MyLinkedList.java
@@ -21,33 +21,75 @@ public class MyLinkedList<T> {
 	ListElement first;
 
 	public int size() {
-		// TODO: Implement
-		return 0;
+	int count= 0;
+	ListElement current =first;
+	while (current!=null) {
+		count++;
+		current= current.next;
+	}
+		return count;
 	}
 
 	public boolean contains(Object o) {
-		// TODO: Implement
+		ListElement current= first;
+		while (current != null ) {
+			if (current.payload == o) {
+				return true;
+			}
+			current= current.next;
+		}
 		return false;
 	}
 
 	public boolean remove(Object o) {
-		// TODO: Implement
+		//Erstes Element entfernt
+		
+		if  (o.equals(first.payload)) {
+			first=first.next;
+			return true;
+		}
+		
+		// Element aus der Mitte entfernt
+     ListElement current = first;
+     while (current.next != null ) {
+			if (o.equals(current.next.payload)) {
+				current.next =current.next.next;
+				return true;
+			}
+				current= current.next;
+			}
 		return false;
 
 	}
 
 	public T set(int index, T element) {
-		// TODO: Implement
-		return element;
+		ListElement node = getElement (index);
+		T oldValue =node.payload;
+		node.payload =element;
+		return oldValue;
 	}
 
 	public void add(int index, T element) {
-		// TODO: Implement
-	}
-
+		ListElement newElement = new ListElement (element);
+		
+		if (index==0) {
+			newElement.next= first;
+			first= newElement;
+		} else {
+		ListElement previous = getElement (index-1);
+		if (previous != null) {
+		newElement.next = previous.next;
+		previous.next= newElement;
+							   }
+		       }
+	   }
 
 	public T remove(int index) {
-		// TODO: Implement
+		  if(index ==0) {
+			  first = first.next; 
+		  }
+		  ListElement previous = getElement(index-1); 
+		  previous.next = previous.next.next; 
 		return null;
 	}
 
