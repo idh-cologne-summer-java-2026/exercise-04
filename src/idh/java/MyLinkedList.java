f5b2b73c2e9f4f212e2940294518fa1673ecf067
Alice <alesya.babkina2000@gmail.com>
2026-05-19 14:07:28 +0200

    Hausaufgaben #4

--- src/idh/java/MyLinkedList.java
+++ src/idh/java/MyLinkedList.java
@@ -21,35 +21,82 @@ public class MyLinkedList<T> {
 	ListElement first;
 
 	public int size() {
-		// TODO: Implement
-		return 0;
+		int count = 0;
+		ListElement current = first;
+		while(current != null) {
+			count++;
+			current = current.next;
+		}
+		return count;
+		
 	}
 
 	public boolean contains(Object o) {
-		// TODO: Implement
+		ListElement current = first;
+		while(current != null) {
+			if(current.payload == o) {
+				return true;
+			}
+			current = current.next;
+		}
+		
 		return false;
 	}
 
 	public boolean remove(Object o) {
-		// TODO: Implement
-		return false;
-
+	 ListElement current = first;
+	 
+	 while(current != null) {
+		 if(o.equals(current.payload)) {
+			 return true;
+		 }
+		 current = current.next;
+	 }
+return false;
 	}
 
 	public T set(int index, T element) {
-		// TODO: Implement
+		ListElement e = getElement(index);
+		e.payload = element;
+		
 		return element;
 	}
 
 	public void add(int index, T element) {
-		// TODO: Implement
+		ListElement newElement = new ListElement (element);
+		
+		if (index == 0) {
+			newElement.next = first;
+			first = newElement;
+		} else {
+		ListElement previous = getElement(index-1);
+		if (previous != null) {
+		  newElement.next = previous.next;
+		  previous.next = newElement;
+		
+ 
+		  }
+		}
 	}
-
-
 	public T remove(int index) {
-		// TODO: Implement
-		return null;
+		if(index == 0) {
+		      T value = first.payload;
+		      first = first.next;
+		      return value;
+			}
+			
+		    ListElement current = first;
+		    
+		    while(index > 1) {
+		    	current = current.next;
+		    	index--;
+				}
+			T value = current.next.payload;
+			current.next = current.next.next;
+		
+			return value;
 	}
+
 
 	public boolean isEmpty() {
 		return first == null;
